6e078adea0bb9e6bbe49c745059eae47a2c449c2
Clonephaze <jackdsmith2327@gmail.com>
2023-06-19 23:38:04 -0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,16 +1,15 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-	if (msg.content === 'ping') {
-		msg.reply('pong');
-	}
-});
-
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-console.log(token);
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
 client.login(token);
