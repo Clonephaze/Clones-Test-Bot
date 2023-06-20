3da28a3e127913084df6ac73575156b7fcf41a26
Clonephaze <jackdsmith2327@gmail.com>
2023-06-19 23:35:35 -0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
- const client = new Discord.Client();
+const client = new Discord.Client();
 
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
+	console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
+	if (msg.content === 'ping') {
+		msg.reply('pong');
+	}
+});
 
-client.login('MTEyMDU4MTA3NTk2MjQzNzcxMg.GvTtsO.2YxQYdfTfpftPqyC0cgnNP48vkiDNkJ43rMJH0');``
+const { token } = require('./config.json');
+console.log(token);
+client.login(token);
