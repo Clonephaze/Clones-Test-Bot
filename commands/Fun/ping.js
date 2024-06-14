c44bcaa468a742de157b153dab9f781c91190bdf
Clonephaze <jackdsmith2327@gmail.com>
2024-06-14 11:39:58 -0700

    Made Ping functional

--- commands/Fun/ping.js
+++ commands/Fun/ping.js
@@ -1,13 +1,22 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 
 	async execute(interaction) {
+		const client = interaction.client;
 
-		await interaction.reply('Pong!');
+		const embed = new EmbedBuilder()
+			.setColor('#0099ff')
+			.setTitle('Pong!')
+			.addFields(
+				{ name: 'Latency', value: `${Date.now() - interaction.createdTimestamp}ms`, inline: true },
+				{ name: 'API Latency', value: `${Math.round(client.ws.ping)}ms`, inline: true },
+			)
+			.setTimestamp();
+
+		await interaction.reply({ embeds: [embed], ephemeral: true });
 	},
 };
